bee6ed8351b4ba0d6ca0a8131b2a28bf0b69d63e
Kuyeso Rogers <kuyeso@halodevs.tech>
2022-05-31 19:34:36 +0000

    setup the routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv')
+const router = require('./routes/goalRoute')
 
 dotenv.config()
 const port = process.env.PORT || 5000
 
-app.get('/api/v1/goals', (req, res) => {
-    res.status(200).json({
-        message: 'Get goals'
-    })
-})
+app.get('/api/v1/goals', router)
 app.listen(port, () => {
     console.log(`Listening on port ${port}`.red.underline)
 })
