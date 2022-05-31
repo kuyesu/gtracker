c2167efd619df0088a8d951fdd5f89f567cab654
Kuyeso Rogers <kuyeso@halodevs.tech>
2022-05-31 19:41:54 +0000

    setup goal controllers and the the goal route structures

---
+++ backend/controllers/goalControllers.js
@@ -0,0 +1,23 @@
+const getGoals = (req, res) => {
+    res.status(200).json({
+        message: 'Get goals'
+    })
+}
+
+const setGoal = (req, res) => {
+    res.status(200).json({
+        message: 'Create goals'
+    })
+}
+
+const updateGoal = (req, res) => {
+    res.status(200).json({
+        message: 'Update goals'
+    })
+}
+
+const deleteGoal = (req, res) => {
+    res.status(200).json({
+        message: 'Delete goals'
+    })
+}
--- backend/routes/goalRoute.js
+++ backend/routes/goalRoute.js
@@ -8,4 +8,28 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    res.status(200).json({
+        message: 'Create goals'
+    })
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({
+        message: 'Update goals'
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({
+        message: 'Delete goals'
+    })
+})
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Get goals'
+    })
+})
+
 module.exports = router
