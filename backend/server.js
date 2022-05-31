d6aebafca6d9c468245f60f22d6d527f7488e525
Kuyeso Rogers <kuyeso@halodevs.tech>
2022-05-31 20:37:15 +0000

    create connection to the database

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const errorHandle = require('./middleware/errorHandle')
 dotenv.config()
 const port = process.env.PORT || 5000
 
+// CONNECT TO DB
+connectDB()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/v1/goals', router)
