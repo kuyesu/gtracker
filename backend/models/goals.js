5e00f459fe1da47916f6faa6fded94b9b5d1c9cc
Kuyeso Rogers <kuyeso@halodevs.tech>
2022-05-31 20:50:05 +0000

    create goal schema

---
+++ backend/models/goals.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const goalScema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description']
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+        required: [true, 'Please add a completed']
+    },
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Please add an owner']
+    },
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('Goal', goalScema)
