eba31b365e6d6b6842e535565dd35953e086e0a4
Kuyeso Rogers <kuyeso@halodevs.tech>
2022-05-31 20:22:26 +0000

    add async handler for the function process

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,41 +1,42 @@
+const asyncHandler = ('express-async-handler');
 // @desc get goals
 // @route GET /api/v1/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler ( async (req, res) => {
     res.status(200).json({
         message: 'Get goals'
     })
-}
+})
 
 // @desc set goals
 // @route POST /api/v1/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler ( async (req, res) => {
     req.body.text
     console.log(req.body.text)
 
     res.status(200).json({
         message: 'Create goals'
     })
-}
+})
 
 // @desc update goals
 // @route PUT /api/v1/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler ( async (req, res) => {
     res.status(200).json({
         message: 'Update goals'
     })
-}
+})
 
 // @desc delete goals
 // @route DELETE /api/v1/goals/:id
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler ( async (req, res) => {
     res.status(200).json({
         message: 'Delete goals'
     })
-}
+})
 
 module.exports = {
     getGoals,
